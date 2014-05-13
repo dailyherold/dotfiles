0fb27abb7b8cca8beea703c21987d2713866dfd4
dailyherold <johnpaul7@gmail.com>
2014-05-13 12:12:38 -0500

    improved swapcaps to refresh xcape as well

--- util/swapcaps.sh
+++ util/swapcaps.sh
@@ -6,3 +6,11 @@ else
   setxkbmap -option ctrl:swapcaps
 fi
 
+# Kill xcape
+pgrep xcape|awk '{print "kill "$1}'|sh 
+
+# Restart xcape
+if [ -z "$(pgrep xcape)" ]; then
+    ~/repos/git/xcape/xcape -e 'Control_L=Escape'
+fi
+
